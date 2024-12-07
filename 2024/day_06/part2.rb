e0f8a7633a08b0948068100e755f67e16debface
Iago Moreira <iago.moreira@on-running.com>
2024-12-07 08:19:37 +0100

    2024 - Day 06 - part 2

--- 2024/day_06/part2.rb
+++ 2024/day_06/part2.rb
@@ -7,64 +7,40 @@ DIRECTIONS_TRANSITIONS = {
   :left => :up
 }
 
+def count_loops map, element, dir, seen, recurse
+  loops = 0
+  loop do
+    next_element = element.adjacent_element_in_direction(dir)
+    next_pos = next_element&.position
+    case map[next_pos]
+    when true
+      if recurse
+        map[next_pos] = false
+        loops += 1 if seen.none? { |_pos, _| _pos == next_pos } &&
+                      count_loops(map, element, dir, seen.clone, false)
+        map[next_pos] = true
+      end
+      element = next_element
+      return true unless seen.add? [element.position, dir]
+    when false
+      dir = DIRECTIONS_TRANSITIONS[dir]
+    when nil
+      return recurse && loops
+    end
+  end
+end
+
 input = ARGF.readlines.map {|line| line.strip.split('') }
 grid = BidimensionalArray.new(input)
-VISITED_POSITIONS = {}
 
-def count_steps(element, direction, visited_positions)
-  return visited_positions if element.nil?
+map = {}
+start_element = nil
 
-  next_position = element.adjacent_element_in_direction(direction)
-  current_position_id = element.position
-
-  if next_position == '#'
-    return count_steps(element, DIRECTIONS_TRANSITIONS[direction], visited_positions)
-  end
-
-  if visited_positions[current_position_id]
-    raise StandardError if visited_positions[current_position_id].include?(direction)
-    return count_steps(next_position, direction, visited_positions)
-  else
-    visited_positions[current_position_id] ||= []
-    visited_positions[current_position_id] += [direction]
-
-    return count_steps(next_position, direction, visited_positions)
-  end
-end
-
-visited_positions = {}
 grid.each do |line|
-  line.each do |char|
-    if char == '^'
-      visited_positions = count_steps(char, :up, visited_positions)
-    end
+  line.each do |element|
+    map[element.position] = element.value != '#'
+    start_element = element if element.value == '^'
   end
 end
 
-puts visited_positions.size
-
-loops_found = 0
-
-visited_positions.each do |position, _|
-  new_grid = grid.dup
-  i, j = position
-  old_element = grid[i][j]
-  if old_element.value != '^'
-    element = BidimensionalArray::Element.new('#', position, new_grid)
-    new_grid[i][j] = element
-  end
-
-  new_grid.each do |line|
-    line.each do |char|
-      if char == '^'
-        begin
-          count_steps(char, :up, {})
-        rescue StandardError
-          loops_found += 1
-        end
-      end
-    end
-  end
-end
-
-puts loops_found
+puts count_loops(map, start_element, :up, Set.new([[start_element.position, 0]]), true)
